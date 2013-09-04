e94bd57330b560cfdb81a4ec23a5fd02dabd4f31
James King <metapyziks@gmail.com>
2013-09-05 00:52:51 +0100

    Added Servlet abstract class

---
+++ Servlet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    public abstract class Servlet
+    {
+        protected HttpListenerRequest Request { get; private set; }
+        protected HttpListenerResponse Response { get; private set; }
+
+        protected StreamWriter Writer { get; private set; }
+
+        public void Service(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            Request = request;
+            Response = response;
+
+            using (Writer = new StreamWriter(Response.OutputStream)) {
+                OnService();
+            }
+        }
+
+        protected abstract void OnService();
+    }
+}

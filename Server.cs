5874e24b5e5bc51a3c10f57c887bfcffc011946b
James King <metapyziks@gmail.com>
2013-09-05 15:00:39 +0100

    Removed unused usings

--- Server.cs
+++ Server.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Reflection;
 using System.Net;
+using System.Reflection;
 
 namespace WebServer
 {
--- Servlet.cs
+++ Servlet.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WebServer
 {

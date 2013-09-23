375ae8024343ed6acdbea5f5467d3e1a9cb79693
James King <metapyziks@gmail.com>
2013-09-23 23:32:55 +0100

    Added GetParsedPost

---
+++ Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    public static class Extensions
+    {
+        public static NameValueCollection GetParsedPost(this HttpListenerRequest request)
+        {
+            // blah=something&other=kayegyjwqe&
+            var body = new StreamReader(request.InputStream).ReadToEnd();
+            var pairs = body.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var output = new NameValueCollection();
+
+            foreach (var pair in pairs) {
+                var nv = pair.Split('=');
+                if (nv.Length == 2) {
+                    output.Add(nv[0], WebUtility.UrlDecode(nv[1]));
+                } else {
+                    output.Add(nv[0], String.Empty);
+                }
+            }
+
+            return output;
+        }
+    }
+}

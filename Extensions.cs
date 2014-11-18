624862eaf2d1b05516cc7b1d290a93396a8a89f2
Ziks <james.king@facepunchstudios.com>
2014-11-18 15:57:30 +0000

    Refactored GetParsedPost().

--- Extensions.cs
+++ Extensions.cs
@@ -2,28 +2,22 @@
 using System.Collections.Specialized;
 using System.IO;
 using System.Net;
+using System.Web;
 
 namespace WebServer
 {
     public static class Extensions
     {
-        public static NameValueCollection GetParsedPost(this HttpListenerRequest request)
+        public static String GetBodyString(this HttpListenerRequest request)
         {
-            var body = new StreamReader(request.InputStream).ReadToEnd();
-            var pairs = body.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
-
-            var output = new NameValueCollection();
-
-            foreach (var pair in pairs) {
-                var nv = pair.Split('=');
-                if (nv.Length == 2) {
-                    output.Add(nv[0], WebUtility.UrlDecode(nv[1]));
-                } else {
-                    output.Add(nv[0], String.Empty);
-                }
+            using (var reader = new StreamReader(request.InputStream)) {
+                return reader.ReadToEnd();
             }
+        }
 
-            return output;
+        public static NameValueCollection GetParsedBody(this HttpListenerRequest request)
+        {
+            return HttpUtility.ParseQueryString(request.GetBodyString());
         }
     }
 }

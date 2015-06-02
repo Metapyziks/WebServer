8a3cbb4469e8ecfeff79a8dc73243af04dfdc240
James King <james.king@facepunchstudios.com>
2015-06-02 11:23:49 +0100

    Added `Content-Range` header.

--- DefaultResourceServlet.cs
+++ DefaultResourceServlet.cs
@@ -85,25 +85,27 @@ namespace WebServer
                     }
 
                     int min = 0, max = int.MaxValue;
-                    var usingRange = false;
 
                     var rangeHeader = Request.Headers["Range"];
                     if (rangeHeader != null && rangeHeader.StartsWith("bytes=")) {
                         rangeHeader = rangeHeader.Substring("bytes=".Length);
                         var split = rangeHeader.Split('-');
                         if (split.Length == 2) {
-                            usingRange = true;
                             if (split[0].Length > 0) int.TryParse(split[0], out min);
                             if (split[1].Length > 0) int.TryParse(split[1], out max);
                         }
                     }
 
                     if (Request.HttpMethod != "HEAD") {
-                        if (usingRange) Response.StatusCode = 206;
-
                         using (var stream = File.OpenRead(path)) {
                             max = Math.Min(max, (int) stream.Length);
+
                             Response.ContentLength64 = max - min;
+
+                            if (min != 0 || max != stream.Length) {
+                                Response.StatusCode = 206;
+                                Response.Headers.Add("Content-Range", string.Format("bytes {0}-{1}/{2}", min, max, stream.Length));
+                            }
 
                             stream.Seek(min, SeekOrigin.Begin);
                             CopyTo(stream, Response.OutputStream, max - min);

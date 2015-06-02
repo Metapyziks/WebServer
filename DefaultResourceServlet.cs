c272c204dbb7772282f67c44249f2c7285a1958e
James King <james.king@facepunchstudios.com>
2015-06-02 11:10:32 +0100

    Having a go at implementing the Range header.

--- DefaultResourceServlet.cs
+++ DefaultResourceServlet.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace WebServer
 {
     public class DefaultResourceServlet : Servlet
     {
+        private const int DefaultCopyBufferSize = 2048;
+        
+        [ThreadStatic]
+        private static byte[] _sDefaultCopyBuffer;
+        
+        private static void CopyTo(Stream from, Stream dest, int length, byte[] buffer = null)
+        {
+            buffer = buffer ?? _sDefaultCopyBuffer ?? (_sDefaultCopyBuffer = new byte[DefaultCopyBufferSize]);
+            var bufferSize = buffer.Length;
+
+            int toRead, read, total = 0;
+            while ((toRead = Math.Min(length - total, bufferSize)) > 0
+                && (read = from.Read(buffer, 0, toRead)) > 0) {
+                dest.Write(buffer, 0, read);
+                total += read;
+            }
+        }
+
         public static readonly Dictionary<String, String> ContentTypes = new Dictionary<string,string>() {
             { ".css", "text/css" },
             { ".js", "application/javascript" },
@@ -64,22 +82,31 @@ namespace WebServer
                         Response.AddHeader("ETag", VersionNonce);
                     }
 
+                    int min = 0, max = int.MaxValue;
+
                     var rangeHeader = Request.Headers["Range"];
-                    if (rangeHeader != null) {
-                        Server.Log("Range: {0}", rangeHeader);
+                    if (rangeHeader != null && rangeHeader.StartsWith("bytes=")) {
+                        rangeHeader = rangeHeader.Substring("bytes=".Length);
+                        var split = rangeHeader.Split('-');
+                        if (split.Length == 2) {
+                            if (split[0].Length > 0) int.TryParse(split[0], out min);
+                            if (split[1].Length > 0) int.TryParse(split[1], out max);
+                        }
                     }
 
                     if (Request.HttpMethod != "HEAD") {
                         using (var stream = File.OpenRead(path)) {
+                            max = Math.Min(max, (int) stream.Length);
+                            Response.ContentLength64 = max - min;
 
-                            Response.ContentLength64 = stream.Length;
-
-                            stream.CopyTo(Response.OutputStream);
+                            stream.Seek(min, SeekOrigin.Begin);
+                            CopyTo(stream, Response.OutputStream, max - min);
                             stream.Flush();
                         }
                     } else {
                         var info = new FileInfo(path);
-                        Response.ContentLength64 = info.Length;
+                        max = Math.Min(max, (int) info.Length);
+                        Response.ContentLength64 = max - min;
                     }
                     return;
                 }
